7d1a686e97900fa505be7ebceeee74968e8034c2
Nicolas Laval <nicolas.laval@insee.fr>
2019-05-20 09:59:20 +0200

    Improve menu

--- src/components/shared/menu/component.js
+++ src/components/shared/menu/component.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Menu, Button } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 import D from 'i18n';
 import './menu.scss';
 
-const CustomMenu = ({ history, location: { pathname } }) => (
-  <Menu size="massive" id="menu">
-    <Menu.Item header>{D.appTitle}</Menu.Item>
-    <Menu.Menu position="right" id="menu-right">
-      <Button
-        size="big"
-        onClick={() => history.push(pathname === '/simple' ? '/advanced' : '/simple')}
-      >
-        {pathname === '/simple' ? D.advancedTitle : D.simpleTitle}
-      </Button>
-    </Menu.Menu>
-  </Menu>
-);
+const CustomMenu = ({ history }) => {
+  const [selected, setSelected] = useState('/simple');
+  const handler = path => {
+    setSelected(path);
+    history.push(path);
+  };
+  return (
+    <Menu size="massive" id="menu">
+      <Menu.Item header>{D.appTitle}</Menu.Item>
+      <Menu.Menu position="right" id="menu-right">
+        <Button
+          className={selected === '/simple' ? 'selected' : ''}
+          size="big"
+          onClick={() => handler('/simple')}
+        >
+          {D.simpleTitle}
+        </Button>
+        <Button
+          className={selected === '/tree' ? 'selected' : ''}
+          size="big"
+          onClick={() => handler('/tree')}
+        >
+          {D.treeTitle}
+        </Button>
+        <Button
+          className={selected === '/advanced' ? 'selected' : ''}
+          size="big"
+          onClick={() => handler('/advanced')}
+        >
+          {D.advancedTitle}
+        </Button>
+      </Menu.Menu>
+    </Menu>
+  );
+};
 
 export default withRouter(CustomMenu);
 
 CustomMenu.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func.isRequired }).isRequired,
-  location: PropTypes.shape({ pathname: PropTypes.string.isRequired }).isRequired,
 };
